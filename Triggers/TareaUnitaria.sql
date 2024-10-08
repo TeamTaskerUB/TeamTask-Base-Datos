3a262f6d5d4e95848b1893e6365e359e683ab710
LucasMartin9 <lucasmar787@gmail.com>
2024-10-08 17:30:44 -0300

    Demasiadas_Actividades
    Estr trigger se dispara cuando un usuario tiene 3 tareas del mismo proyecto y se le trata de añadir otra

--- Triggers/TareaUnitaria.sql
+++ Triggers/TareaUnitaria.sql
@@ -1,19 +1,4 @@
 
-
-DELIMITER //
-
-CREATE TRIGGER Demasiadas_Actividades
-BEFORE INSERT ON TareaUnitaria
-FOR EACH ROW
-BEGIN
-    IF (SELECT COUNT(*) FROM TareaUnitaria WHERE usuario = NEW.usuario) >= 3 THEN
-        SIGNAL SQLSTATE '45000'
-        SET MESSAGE_TEXT = 'El usuario ya tiene el número máximo de tareas asignadas';
-    END IF;
-END//
-
-DELIMITER ;
-
 DELIMITER $$
 
 CREATE TRIGGER desvincular_tarea
@@ -25,5 +10,32 @@ BEGIN
     AND `grupo` = OLD.grupo 
     AND `Grupo_Proyecto` = (SELECT `proyecto` FROM `Grupo` WHERE `idGrupo` = OLD.grupo);
 END $$
+
+DELIMITER ;
+
+DELIMITER //
+
+CREATE TRIGGER Demasiadas_Actividades
+BEFORE INSERT ON TareaUnitaria
+FOR EACH ROW
+BEGIN
+    DECLARE total_tareas INT;
+
+    SELECT COUNT(*) INTO total_tareas
+    FROM TareaUnitaria 
+    WHERE usuario = NEW.usuario
+    AND grupo = NEW.grupo
+    AND (
+        (NEW.dateIn BETWEEN dateIn AND dateEnd) OR
+        (NEW.dateEnd BETWEEN dateIn AND dateEnd) OR
+        (dateIn BETWEEN NEW.dateIn AND NEW.dateEnd) OR
+        (dateEnd BETWEEN NEW.dateIn AND NEW.dateEnd)
+    );
+
+    IF total_tareas >= 3 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'El usuario ya tiene el número máximo de tareas asignadas en la misma franja de tiempo para este proyecto';
+    END IF;
+END//
 
 DELIMITER ;
