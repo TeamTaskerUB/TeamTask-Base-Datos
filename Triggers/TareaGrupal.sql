1db4f3ad306b10832e84b796a62ca9556048586e
LucasMartin9 <lucasmar787@gmail.com>
2024-10-22 20:58:31 -0300

    Trigger Delete_TareaUnitaria
    Este trigger se dispara cuando se elimina una tarea grupal. Lo que hace es eliminar todas las tareas unitarias asociadas a esa tarea grupal.

--- Triggers/TareaGrupal.sql
+++ Triggers/TareaGrupal.sql
@@ -6,12 +6,11 @@ FOR EACH ROW
 BEGIN
   DECLARE proyecto_duracion INT;
 
-  -- Obtener la duración del proyecto asociado
+
   SELECT TIMESTAMPDIFF(DAY, dateIn, dateEnd) INTO proyecto_duracion
   FROM TareaGlobal
   WHERE idProyecto = NEW.idProyecto;
 
-  -- Verificar si la duración de la tarea grupal excede la del proyecto
   IF NEW.duracion > proyecto_duracion THEN
     SIGNAL SQLSTATE '45000' 
     SET MESSAGE_TEXT = 'La duración de la tarea grupal excede el límite de tiempo del proyecto.';
@@ -20,6 +19,16 @@ END$$
 
 DELIMITER ;
 
+DELIMITER //
 
+CREATE TRIGGER Eliminar_Tareas_Asociadas
+AFTER DELETE ON TareaGrupal
+FOR EACH ROW
+BEGIN
+    DELETE FROM TareaUnitaria
+    WHERE grupo = OLD.idGrupo;
+END//
+
+DELIMITER ;
 
 
