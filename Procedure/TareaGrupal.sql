2e21cc40ce735ef1ea405c3c404dfa37096b416f
Tomás Varas Oviedo <88008465+TomyVarasOviedo@users.noreply.github.com>
2024-10-25 18:25:17 -0300

    Arreglo:[Procedure repetido]

--- Procedure/TareaGrupal.sql
+++ Procedure/TareaGrupal.sql
@@ -1,23 +1,5 @@
 DELIMITER //
 
-CREATE PROCEDURE get_usuarios_proyecto(
-    IN idTareaGlobal INT
-)
-BEGIN
-    SELECT u.idUsuario, u.nombre, u.apellido
-    FROM teamtasker.Usuario u
-    INNER JOIN teamtasker.TareaGrupal_has_Usuario tgu
-        ON u.idUsuario = tgu.usuario
-    INNER JOIN teamtasker.TareaGrupal tg
-        ON tg.idGrupo = tgu.grupo
-    WHERE tg.idProyecto = idTareaGlobal
-    ORDER BY u.nombre;
-END //
-
-DELIMITER ;
-
-DELIMITER //
-
 CREATE PROCEDURE get_grupos_usuario_proyecto (
     IN idUsuario INT,
     IN idTareaGlobal INT
